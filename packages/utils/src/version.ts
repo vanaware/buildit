b14b18ad5129d66f213608ef9e3bda8608ae274a
Ademar Arvati <arvati@hotmail.com>
2026-09-21 14:26:00 -0300

    chore: bump version and update server tasks
    - Increment project version to 0.3.21.
    - Remove redundant --env-file flag from server tasks.
    - Include schema files in utils package publication.

--- packages/utils/src/version.ts
+++ packages/utils/src/version.ts
@@ -4,4 +4,4 @@ declare const __APP_VERSION__: string;
 /** Current library/application version. */
 export const APP_VERSION: string = typeof __APP_VERSION__ !== "undefined"
   ? __APP_VERSION__
-  : "0.3.20#mub46s6d";
+  : "0.3.21#mubi58qq";
