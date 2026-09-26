759209419cec9bfe724a5054dcb601b046d33b8d
Ademar Arvati Filho <arvati@hotmail.com>
2026-09-26 15:34:07 -0300

    error de esbuilt compilando esbuild

--- packages/utils/src/version.ts
+++ packages/utils/src/version.ts
@@ -4,4 +4,4 @@ declare const __APP_VERSION__: string;
 /** Current library/application version. */
 export const APP_VERSION: string = typeof __APP_VERSION__ !== "undefined"
   ? __APP_VERSION__
-  : "0.3.30#muippegu";
+  : "0.3.34#muiq9pmb";
